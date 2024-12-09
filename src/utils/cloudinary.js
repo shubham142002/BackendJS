06558757684368bf0935358cfd884ba7234516d0
Shubham Patel <shubhamparam123@gmail.com>
2024-12-09 11:20:17 +0530

    add multer middleware and solve some mistak in utils, also add controller and routes

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({
+    cloud_name:CLOUDINARY_CLOUD_NAME,
+    api_key:CLOUDINARY_API_KEY,
+    api_secret:CLOUDINARY_API_SECRET
+}); 
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+        cloudinary.uploader.upload(localFilePath,{
+            resource_type: "auto"
+        })
+        console.log("File is uploaded on cloudinary", response.url);
+        return response;
+                
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+}
+
+export {uploadOnCloudinary}
